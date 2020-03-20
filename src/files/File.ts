86fd931fc58e098ffc1dcf470c711b33b276f4a7
Melvin Mupondori <melmups@outlook.com>
2020-03-20 14:14:51 +0200

    Used the same method names as in the node fs library for a bit of consitency

--- src/files/File.ts
+++ src/files/File.ts
@@ -1,13 +1,13 @@
-import {writeFile} from "fs"
+import {writeFile as _writeFile} from "fs"
 
 /**
  * Asynchronously writes data to a file, replacing the file if it already exists. data can be a string or a buffer.
  * @param fileName  filename or file descriptor
  * @param data Data to write to file
  */
-const write = (fileName: string | Buffer, data: string | Buffer | Uint8Array): Promise<void> => {
+const writeFile = (fileName: string | Buffer, data: string | Buffer | Uint8Array): Promise<void> => {
     return new Promise<void>((resolve, reject) => {
-        writeFile(fileName, data, (err: any) => {
+        _writeFile(fileName, data, (err: any) => {
             if (err) {
                 reject(err);
             } else {
