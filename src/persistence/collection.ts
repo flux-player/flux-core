460960d2d2ad486f073ef190efa4727b9396d026
Melvin Mupondori <melmups@outlook.com>
2020-03-24 17:54:26 +0200

    Updated collection base with base suppport for loading configuration from stored collection

--- src/persistence/collection.ts
+++ src/persistence/collection.ts
@@ -1,17 +1,42 @@
 import Structure from "./structure";
-import dotenv from "dotenv"
+import {env} from "../config/configuration"
+import {getAppDataDirectory} from "../files/directory";
 
 export default abstract class Collection {
-    name: string;
-
-    structure: Structure;
-
+    /**
+     * Collection constructor
+     */
     constructor() {
+        // Set the defaults
         this.setDefaultConfiguration();
     }
 
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get structure(): Structure {
+        return this._structure;
+    }
+
+    set structure(value: Structure) {
+        this._structure = value;
+    }
+
+    init() {
+        if (!this._name) throw new Error("A collection should have a name");
+        if (!this._structure) throw new Error("No structure has been defined for this collection");
+
+        this._filename = btoa(this.name);
+    }
+
     setDefaultConfiguration() {
-        this._autoPersist = process.env.PERSIST_MODE === 'auto';
+        this._autoPersist = env('PERSIST_MODE') === 'auto';
+        this._storageDirectory = env('PERSIST_DIRECTORY') ?? getAppDataDirectory('FluxCollections');
     }
 
     /**
@@ -31,6 +56,15 @@ export default abstract class Collection {
     }
 
     private _isLoaded = false;
-
     private _autoPersist = false;
+    private _storageDirectory = "";
+    /**
+     * The name of the collection
+     */
+    private _name: string;
+
+    /**
+     * The structure of the collection (kinda like the schema in a database)
+     */
+    private _structure: Structure;
 }
