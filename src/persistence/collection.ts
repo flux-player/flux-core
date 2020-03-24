f2d73a19d474496149be8b26298cdf76284919c1
Melvin Mupondori <melmups@outlook.com>
2020-03-24 17:27:44 +0200

    Initial structure for automagically persiting collections

--- src/persistence/collection.ts
+++ src/persistence/collection.ts
@@ -1,9 +1,18 @@
 import Structure from "./structure";
+import dotenv from "dotenv"
 
 export default abstract class Collection {
     name: string;
 
     structure: Structure;
+
+    constructor() {
+        this.setDefaultConfiguration();
+    }
+
+    setDefaultConfiguration() {
+        this._autoPersist = process.env.PERSIST_MODE === 'auto';
+    }
 
     /**
      * Run a query against the collection
@@ -13,5 +22,15 @@ export default abstract class Collection {
 
     }
 
+    load() {
+
+    }
+
+    persist() {
+
+    }
+
     private _isLoaded = false;
+
+    private _autoPersist = false;
 }
