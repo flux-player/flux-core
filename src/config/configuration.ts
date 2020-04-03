55727750860db468b14ad73896f1e5cdcbc14445
Melvin Mupondori <melmups@outlook.com>
2020-04-03 03:00:02 +0200

    Added utils and collections as dependencies

--- src/config/configuration.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Fetch a configuration value from environment variables
- *
- * @param key
- * @param _default
- */
-export const env = (key: string, _default = null) => {
-    return typeof process.env[key] ? process.env[key] : _default;
-};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-export {walk} from "./files/directory"
 export {read} from "./audio/metadata/id3"
-export {readFileAsArrayBuffer} from "./extensions/buffer"
-
-export {default as SongCollection} from "./persistence/collections/songs"
-export {default as Structure} from "./persistence/structure"
