950846a60d72437083731e30a921402ee851abf1
Melvin Mupondori <melmups@outlook.com>
2020-04-08 20:39:26 +0200

    Exported createSongFromTags class

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export {default as AudioPlayer} from "./audio/audio";
 export {default as MusicPlayer} from "./audio/player";
 export {default as Song} from "./store/models/audio/song";
 export {MediaScanner} from "./library/scanner";
+
+
+export {createSongFromTags} from "./utils/audio";
