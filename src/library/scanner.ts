fc9c3f76c29615371d9817a70047751f304176e4
Melvin Mupondori <melmups@outlook.com>
2020-04-08 20:41:58 +0200

    Cleaned up MediaScanner class

--- src/library/scanner.ts
+++ src/library/scanner.ts
@@ -4,70 +4,66 @@ import { read } from "../audio/metadata/id3";
 import { createSongFromTags } from "../utils/audio";
 
 export class MediaScanner {
-    /**
-     * The directories to scan media files in
-     */
-    watchlist: Array<string>;
+  /**
+   * The directories to scan media files in
+   */
+  watchlist: Array<string>;
 
-    /**
-     * Create a new instance of the MediaScanner class
-     * @param watchlist The directories to scan media files in
-     */
-    constructor(watchlist: Array<string> = []) {
-        this.watchlist = watchlist;
+  /**
+   * Create a new instance of the MediaScanner class
+   * @param watchlist The directories to scan media files in
+   */
+  constructor(watchlist: Array<string> = []) {
+    this.watchlist = watchlist;
 
-        if(env('FLUX_SCAN_MODE', 'manual') !== 'auto') return;
+    if (env("FLUX_SCAN_MODE", "manual") !== "auto") return;
 
-        // Watch the watchlist for changes and add the new items to the library
-        this.watch();
-    }
-    
-    public async scan() : Promise<Array<Song>> {
-        if(!this.watchlist.length) [];
+    // Watch the watchlist for changes and add the new items to the library
+    this.watch();
+  }
 
-        // Temporary store
-        let filelist: string[] = [];
+  public async scan(): Promise<Array<Song>> {
+    if (!this.watchlist.length) [];
 
-        // Iterate over out watch list
-        for(let i = 0; i < this.watchlist.length; i++) {
-            let directory = this.watchlist[i];
+    // Temporary store
+    let filelist: string[] = [];
 
-            // Walk the directories and get all files matching the specified pattern
-            let files = await walk(directory, ['.mp3']);
+    // Iterate over out watch list
+    for (let i = 0; i < this.watchlist.length; i++) {
+      let directory = this.watchlist[i];
 
-            // Push the master list
-            filelist.push(...files)
-        }
+      // Walk the directories and get all files matching the specified pattern
+      let files = await walk(directory, [".mp3"]);
 
-
-        return this.bulkReadMediaDetails(filelist);
+      // Push the master list
+      filelist.push(...files);
     }
 
-    /**
-     * Read the details for the specified tracks
-     * 
-     * @param filelist The list of files to read
-     */
-    private async bulkReadMediaDetails(filelist: string[]): Promise<Song[]> {
-        let songs: Song[] = [];
-        
-        filelist.forEach(async (file) => {
-            let details = await read(
-                await readFileAsArrayBuffer(file)
-            );
+    return this.bulkReadMediaDetails(filelist);
+  }
 
-            songs.push(createSongFromTags(details, file));
-        });
+  /**
+   * Read the details for the specified tracks
+   *
+   * @param filelist The list of files to read
+   */
+  private async bulkReadMediaDetails(filelist: string[]): Promise<Song[]> {
+    let songs: Song[] = [];
 
-        return songs;
-    }
+    filelist.forEach(async (file) => {
+      let details = await read(await readFileAsArrayBuffer(file));
 
-    /**
-     * Watches directories in the watchlist for changes and if there are any new media files, add them to the 
-     * media library
-     */
-    private watch() {
-        throw new Error("Method not implemented.");
-    }
+      songs.push(createSongFromTags(details, file));
+    });
 
+    return songs;
+  }
+
+  /**
+   * Watches directories in the watchlist for changes and if there are any new media files, add them to the
+   * media library
+   */
+  private watch() {
+    throw new Error("Method not implemented.");
+  }
 }
