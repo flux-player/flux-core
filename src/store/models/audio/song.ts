7c8387214302ce2d470e4ce3c25223b6d38cc116
Melvin Mupondori <melmups@outlook.com>
2020-04-08 20:32:38 +0200

    Added other fields to song tag parsing logic

--- src/store/models/audio/song.ts
+++ src/store/models/audio/song.ts
@@ -87,6 +87,40 @@ export default class Song extends BaseModel {
         case "TIT2":
           song.title = frame.value
           break;
+          case "TIT2":
+            song.title = frame.value;
+              break;
+          case "TPE1":
+            song.artist = frame.value;
+              break;
+          case "TPE2":
+            song.albumArtist = frame.value;
+              break;
+          case "TALB":
+            song.album = frame.value;
+              break;
+          case "TCON":
+            song.genre = frame.value;
+              break;
+          case "TYER":
+            song.year = frame.value;
+              break;
+          case "TPUB":
+            song.publisher = frame.value;
+              break;
+          case "TRCK":
+            song.trackTotal = frame.value;
+              break;
+          case "TPOS":
+            song.trackNumber = frame.value;
+              break;
+
+          case "APIC":
+              // TODO: Image needs to be saved somewhere then we can reference the file on disk instead.
+              //  Storing the actual image data in memory will swallow up memory when the library is big enough
+              //  Alternatively, album art can be read on the fly
+              song.albumArt = "." ?? frame.value;
+              break;
       }
     });
     song.fileName = filename;
