0a3ea042f30b837b755c100733a4c5febfb35a74
Melvin Mupondori <melmups@outlook.com>
2020-04-10 11:47:12 +0200

    Added event bus as parameter

--- src/library/library.ts
+++ src/library/library.ts
@@ -1,19 +1,25 @@
 import { MediaScanner } from "./scanner";
 import SongsCollection from "../store/collections/songs";
+import PlaylistCollection from "../store/collections/playlists";
+import { EventBus } from "@flux/utils";
 
 export class Library {
     scanner: MediaScanner;
     songCollection: SongsCollection;
     playlistCollection: PlaylistCollection;
+    eventBus: EventBus;
 
     constructor(
         scanner: MediaScanner,
         songCollection: SongsCollection,
         playlistCollection: PlaylistCollection,
-
+        eventBus: EventBus
     ) {
         this.scanner = scanner;
         this.songCollection = songCollection;
         this.playlistCollection = playlistCollection;
+        this.eventBus = eventBus ?? new EventBus();
     }
+
+
 }
