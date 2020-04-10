d6a9588333236277ef7b0b2cec2ad029821f5616
Melvin Mupondori <melmups@outlook.com>
2020-04-10 11:55:32 +0200

    Added listener for first application event

--- src/library/library.ts
+++ src/library/library.ts
@@ -1,12 +1,27 @@
-import { MediaScanner } from "./scanner";
-import SongsCollection from "../store/collections/songs";
-import PlaylistCollection from "../store/collections/playlists";
 import { EventBus } from "@flux/utils";
+import PlaylistCollection from "../store/collections/playlists";
+import SongsCollection from "../store/collections/songs";
+import { MediaScanner } from "./scanner";
 
 export class Library {
-    scanner: MediaScanner;
-    songCollection: SongsCollection;
-    playlistCollection: PlaylistCollection;
+    /**
+     * Media scanner for reading media so we can add it into our library
+     */
+    private scanner: MediaScanner;
+
+    /**
+     * A collection of songs in our library
+     */
+    private songCollection: SongsCollection;
+
+    /**
+     * A collection of playlists in our library
+     */
+    private playlistCollection: PlaylistCollection;
+
+    /**
+     * Event bus for broadcasting events to the other components in the application
+     */
     eventBus: EventBus;
 
     constructor(
@@ -19,7 +34,17 @@ export class Library {
         this.songCollection = songCollection;
         this.playlistCollection = playlistCollection;
         this.eventBus = eventBus ?? new EventBus();
+
+        this.eventBus.listen('application.launches.first', this.onFirstRun);
     }
 
 
+    /**
+     * This is an event listener for when the application has been launched for the first time
+     *
+     * It will do it's initial scan of the user's music library
+     */
+    private onFirstRun() {
+        this.scanner.scan();
+    }
 }
