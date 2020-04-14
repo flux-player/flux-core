bad2f439e3ca8ce8bb096baa56916614de722203
Melvin Mupondori <melmups@outlook.com>
2020-04-14 16:11:04 +0200

    Exported createQuickPlaylist method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Library, MediaScanner } from "./library";
 export { AudioPlayer, MusicPlayer } from "./audio";
 
-export { createSongFromTags } from "./utils/audio";
+export { createSongFromTags, createQuickPlaylist } from "./utils/audio";
 
 export { Song, Playlist } from "./store/models/audio";
 export { SongCollection, PlaylistCollection } from "./store/collections";
