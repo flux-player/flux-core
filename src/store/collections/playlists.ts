8ae0484729923a51e4246f3828fc4abe8ec148c0
Melvin Mupondori <melmups@outlook.com>
2020-04-14 21:44:35 +0200

    Cleaned up PlaylistCollection class

--- src/store/collections/playlists.ts
+++ src/store/collections/playlists.ts
@@ -1,17 +1,27 @@
-import { Column, Structure, Collection } from "@flux/collections";
 import Playlist from "../models/audio/playlist";
+import { Column, Structure, Collection } from "@flux/collections";
 
 export default class PlaylistCollection extends Collection<Playlist> {
+
+    /**
+     * Create a new instance of the playlist collection
+     */
     constructor() {
+        // Call base constructor
         super();
 
+        // Initialize the collection
         this.init();
     }
 
-    // The name of the collection
+    /**
+     * The name of the collection
+     */
     _name = "playlists";
 
-    // Define the structure of the collection
+    /**
+     * Define the structure of the collection
+     */
     _structure = new Structure([
         new Column("name", "string", true),
         new Column("createdAt", "string", true),
