73ef95f2bae3af7b5cae4d2186fed8a66ca38285
Melvin Mupondori <melmups@outlook.com>
2020-04-14 21:42:13 +0200

    Cleaned up Song class

--- src/store/models/audio/song.ts
+++ src/store/models/audio/song.ts
@@ -5,47 +5,72 @@ export default class Song extends BaseModel {
      * The title of the song
      */
     public title: string;
+
     /**
      * The name of the artist of the song
      */
     public artist: string;
+
     /**
      * The name of the album of the song
      */
     public album: string;
+
     /**
      * The artist of the album
      */
     public albumArtist: string;
+
     /**
      * The number of the song in the album
      */
     public trackNumber: number;
+
     /**
      * The total tracks in the album
      */
     public trackTotal: number;
+
     /**
      * Full path to the album art file
      */
     public albumArt: string;
+
     /**
      * The genre of the song
      */
     public genre: string;
+
     /**
      * The year the song was released
      */
     public year: string;
+
     /**
      * The name of publisher of the song
      */
     public publisher: string;
+
     /**
      * The full path to the song file
      */
     public fileName: string;
 
+    /**
+     * Create a new Song instance
+     *
+     * @param title The title of the song, default = ""
+     * @param artist The artist of the song, default = ""
+     * @param album The album of the song, default = ""
+     * @param albumArtist The artist of the song album, default = ""
+     * @param trackNumber The position of the song in its album, default = "0"
+     * @param trackTotal The total numer of songs in the song's album, default = "0"
+     * @param albumArt The album art of the song, default = ""
+     * @param genre The genre of the song, default = ""
+     * @param year The year the song was released, default = ""
+     * @param publisher The publisher of the song, default = ""
+     * @param fileName The file name of the song, default = ""
+     */
     constructor(
         title: string = "",
         artist: string = "",
