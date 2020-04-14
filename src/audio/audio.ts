aae53d15a6deadae2c937357c55f898fcb511289
Melvin Mupondori <melmups@outlook.com>
2020-04-14 21:59:26 +0200

    Removed audio progress from audio player file

--- src/audio/audio.ts
+++ src/audio/audio.ts
@@ -1,19 +1,4 @@
-export interface AudioProgress {
-    /**
-     * The position the player is at in the track, in seconds
-     */
-    position: number;
-
-    /**
-     * The duration of the track currently being played
-     */
-    duration: number;
-
-    /**
-     * Showing, 0 - 100 how far into the track we are
-     */
-    percentage: number;
-}
+import { AudioProgress } from "./AudioProgress";
 
 export default class AudioPlayer {
     /**
