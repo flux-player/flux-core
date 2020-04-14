9a822c36cb33cc4416e54564bc016c2cbca14d47
Melvin Mupondori <melmups@outlook.com>
2020-04-14 21:39:27 +0200

    Cleaned up playlist class

--- src/store/models/audio/playlist.ts
+++ src/store/models/audio/playlist.ts
@@ -9,15 +9,24 @@ export default class Playlist extends BaseModel {
      * The name of the playlist
      */
     public name: string;
+
     /**
      * Unix timestamp for when the playlist was created
      */
     public createdAt: Number;
+
     /**
      * Collection of songs on the playlist
      */
     public songs: Song[];
 
+    /**
+     * Create a new playlist instance
+     *
+     * @param name The name of the playlist
+     * @param createdAt Unix timestamp of when the playlist was created
+     * @param songs The array of songs in the playlist
+     */
     constructor(name: string, createdAt: Number, songs: Song[]) {
         super();
 
@@ -26,10 +35,20 @@ export default class Playlist extends BaseModel {
         this.songs = songs;
     }
 
+    /**
+     * Gets the index of the specified song in the playlist
+     *
+     * @param song The song to get the index for
+     */
     public getSongPosition(song: Song) {
         return this.songs.indexOf(song);
     }
 
+    /**
+     * Gets the song at the specified index in the playlist
+     *
+     * @param index The index to get the song
+     */
     public getAtPosition(index: number): Song {
         return this.songs[index];
     }
