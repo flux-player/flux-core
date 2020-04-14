7b16cb4439fc5ccc8d379e106e671a5509f0ed8c
Melvin Mupondori <melmups@outlook.com>
2020-04-14 16:08:37 +0200

    Added a helper method to create a playlist from an array of songs

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -1,5 +1,14 @@
 import { Song } from "..";
 import { ID3TagCollection, ID3Frame } from "../audio/metadata/id3";
+import { Playlist } from "../store/models/audio";
+
+/**
+ * Create a quick playlist from a list of songs
+ * @param songs
+ */
+export function createQuickPlaylist(songs: Array<Song>) : Playlist {
+   return new Playlist("Playlist", Date.now(), songs);
+}
 
 /**
  * Create a new song from the given tags
